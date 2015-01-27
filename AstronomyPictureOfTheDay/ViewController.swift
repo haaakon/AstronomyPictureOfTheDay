2d047e9b3e6237ab93f9edbb8824d2bb7cd0cc57
Håkon Bogen <hakon.bogen@shortcut.no>
2015-01-27 19:49:25 +0100

    Initial Commit

---
+++ AstronomyPictureOfTheDay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AstronomyPictureOfTheDay
+//
+//  Created by Håkon Bogen on 27/01/15.
+//  Copyright (c) 2015 hakonbogen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
